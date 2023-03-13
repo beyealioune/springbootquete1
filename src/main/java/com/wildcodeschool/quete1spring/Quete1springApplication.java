e6d75c9523d0f08e53c51c08326402a2177c14c6
BEYE ALIOUNE <alioune.beye@gmail.com>
2023-03-13 16:28:38 +0100

    quete1

---
+++ src/main/java/com/wildcodeschool/quete1spring/Quete1springApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.quete1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quete1springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quete1springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/quete1spring/Quete1springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.quete1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quete1springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
